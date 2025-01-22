002f6b02c1f4089631f868306dccc64150570989
SchBenedek <benedek.schlesser@gmail.com>
2025-01-22 12:03:41 +0100

    seed --> different subjects complete

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,39 +6,30 @@ const prisma = new PrismaClient();
 async function main() {
     const subjects = ["Matematika", "Történelem", "Kémia", "Informatika", "Irodalom"];
     const assignments = [];
+    const r=Math.floor(Math.random()*100);
 
-    for (let i=0; i<10; i++){
-      for (const subject of subjects) {
-        let assignment = await prisma.assignment.findFirst({
-          where: { subject },
-        });
-    
-        if (!assignment) {
-          assignment = await prisma.assignment.create({
-            data: {
-              subject: subject,
+    for (let i = 0; i < 100; i++) {
+      const randomSubject = faker.helpers.arrayElement(subjects); 
+      const assignment = await prisma.assignment.create({
+          data: {
+              subject: randomSubject,
               ageGroup: faker.helpers.arrayElement(["alsos", "felsos", "kozep_isk", "felso_okt"]),
               assignments: faker.lorem.lines(),
-            },
-          });
-          console.log(`Created assignment: ${JSON.stringify(assignment)}`);
-        } else {
-          console.log(`Found existing assignment: ${JSON.stringify(assignment)}`);
-        }
-    
-        assignments.push(assignment);
-      }
+          },
+      });
+      assignments.push(assignment); 
+      console.log(`Created assignment for ${randomSubject}:`, assignment);
     }
 
     for (let i = 0; i < 20; i++) {
         const randomAssignment = faker.helpers.arrayElement(assignments);
 
-        const fName=faker.person.firstName;
-        const lName=faker.person.lastName;
+        const fName = faker.person.firstName();
+        const lName = faker.person.lastName();
 
         const teacher = await prisma.teacher.create({
             data: {
-                name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                name: `${fName} ${lName}`,
                 subjectTeacher: randomAssignment.subject,
                 hourlyRate: faker.number.int({ min: 2000, max: 10000 }),
                 email: `${fName}.${lName}@citromail.com`,
