0e73b8dbe2a0155ddaf3ba4a8736af08cb80812c
BenedekBogardi <bogardi.benedek@petrik.hu>
2025-02-04 19:56:39 +0100

    Swagger base added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Final Exam backend documentation')
+    .setDescription('This is a detailed documentation of the website made for the final exam.')
+    .setVersion('1.0')
+    .addTag('teaching')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
