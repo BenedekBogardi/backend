b35d8055df085a642d40fde7843686146f6b8ade
Petrik Diak <diak@petrik.hu>
2025-02-05 14:13:04 +0100

    Auth fixed, prettier removed, API docs started - Commit by Bogárdi Benedek

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TeachersModule } from './teachers/teachers.module';
 import { StudentsModule } from './students/students.module';
 import { AssignmentsModule } from './assignments/assignments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TeachersModule, StudentsModule, AssignmentsModule],
+  imports: [TeachersModule, StudentsModule, AssignmentsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,15 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UnauthorizedException, UseGuards, Request } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
 import { LoginDto } from './dto/login-dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiHeader, ApiOkResponse, ApiBody, ApiProperty, ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @Controller('teachers')
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
   @Post()
+  @ApiBody({ type: [CreateTeacherDto] })
   create(@Body() createTeacherDto: CreateTeacherDto) {
     return this.teachersService.create(createTeacherDto);
   }
@@ -23,6 +26,30 @@ export class TeachersController {
       }
     }
   
+  @ApiProperty({
+    type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Error'
+        },
+        subjectTeacher: {
+          type: 'string',
+          example: "Történelem"
+        }
+    },
+    required: ['name', 'subjectTeacher']
+   })
+  @ApiTags()
+  @ApiBody({
+    description: "List all of the teachers.",
+  })
+  @ApiResponse({ status: 201, description: 'Teacher has been succesfully added.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.'})
+  /*@ApiHeader({
+    name: 'Get all teachers'
+  })*/
+  //may not be required
   @Get()
   @UseGuards(AuthGuard('bearer'))
     findAll(@Request() request) {
