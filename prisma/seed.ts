d1aa6c7d8abb79a7c4e3e15885b78ae67db2e3e9
Benedek Schlesser <benedek.schlesser@gmail.com>
2025-03-16 13:12:19 +0100

    seed fixed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,67 +6,94 @@ const prisma = new PrismaClient();
 
 async function main() {
     const subjects = Object.values($Enums.Subjects);
-    const ageGroups = Object.values($Enums.Level)
+    const ageGroups = Object.values($Enums.Level);
     const assignments = [];
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash("alma", saltRounds);
 
-    
-    for (let i = 0; i < 20; i++) {
-        const randomAssignment = faker.helpers.arrayElement(assignments);
-        const fName = faker.person.firstName();
-        const lName = faker.person.lastName();
-        
-        await prisma.teacher.create({
-            data: {
-                hourlyRate: faker.number.int({min: 1000, max:90000}),
-                user: {
-                    connect: {
-                        id: i
-                    }
+    const users = await Promise.all(
+        Array.from({ length: 20 }).map(async () => {
+            var firstN = faker.person.firstName();
+            var lastN = faker.person.lastName();
+            return prisma.user.create({
+                data: {
+                    firstName: firstN,
+                    lastName: lastN,
+                    email: `${lastN}.${firstN}@citromail.com`,
+                    password: await bcrypt.hash("password123", saltRounds),
                 },
-                rating: faker.number.float({min: 1, max: 5, fractionDigits: 1}),
-                subject: faker.helpers.arrayElement(subjects)
-            },
-        });
-    }
-    
-   
+            });
+        })
+    );
+
+    const teachers = await Promise.all(
+        users.map(async (user) => {
+            return prisma.teacher.create({
+                data: {
+                    hourlyRate: faker.number.int({ min: 1000, max: 90000 }),
+                    user: {
+                        connect: {
+                            id: user.id,
+                        },
+                    },
+                    rating: faker.number.float({ min: 1, max: 10, fractionDigits: 1 }),
+                    subject: faker.helpers.arrayElement(subjects),
+                },
+            });
+        })
+    );
+
+    const students = await Promise.all(
+        users.map(async (user) => {
+            return prisma.student.create({
+                data: {
+                    user: {
+                        connect: {
+                            id: user.id,
+                        },
+                    },
+                    ageGroup: faker.helpers.arrayElement(ageGroups),
+                }
+            })
+        })
+    )
+
     for (let i = 0; i < 100; i++) {
-        const randomSubject = faker.helpers.arrayElement(subjects);
+        const randomTeacher = faker.helpers.arrayElement(teachers);
         const assignment = await prisma.assignment.create({
             data: {
-                subject: randomSubject,
+                subject: faker.helpers.arrayElement(subjects),
                 ageGroup: faker.helpers.arrayElement(ageGroups),
                 description: faker.person.bio(),
-                name: faker.science.chemicalElement().name + "előállítás feladat",
+                name: faker.science.chemicalElement().name + " előállítás feladat",
                 teacher: {
                     connect: {
-                        id: i%20
-                    }
-                }
-            }
+                        id: randomTeacher.id,
+                    },
+                },
+            },
         });
         assignments.push(assignment);
     }
-    
+
     await prisma.admin.create({
         data: {
             name: faker.person.fullName(),
             email: `${faker.person.firstName()}@citromail.com`,
-            password: "alma"
-        }
-    })
+            password: hashedPassword,
+        },
+    });
+
+    console.log("Database seeded successfully!");
 }
 
 main()
-  .then(async () => {
-    console.log('Database seeded successfully!');
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
