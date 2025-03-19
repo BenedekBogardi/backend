c8aeae398b86da4546d672722b7fb90ac92357a8
Benedek Schlesser <benedek.schlesser@gmail.com>
2025-03-19 21:27:29 +0100

    auth login id bug fixed

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -3,6 +3,7 @@ import {createParamDecorator, ExecutionContext} from "@nestjs/common";
 export const Self = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const req = ctx.switchToHttp().getRequest();
-        return req.user.id;
+        console.log("ctx id:", req.user.userId)
+        return req.user.userId;
     }
 )
